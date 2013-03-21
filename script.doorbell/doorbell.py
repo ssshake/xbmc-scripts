900828bed3732e6e00bd3bb84d3b451d868b5989
shake <git@doobiest.net>
2013-03-21 18:36:33 -0400

    update paths for cross platform

--- script.doorbell/doorbell.py
+++ script.doorbell/doorbell.py
@@ -1,25 +1,47 @@
 # Import the XBMC/XBMCGUI modules.
-import xbmc, xbmcgui, time, urllib
+import xbmc, xbmcgui, time, urllib, xbmcvfs, xbmcaddon, os
+__addon__   = xbmcaddon.Addon()
+__addonid__ = __addon__.getAddonInfo('id')
 
-#inherit from WindowDialog instead of Window so that it's shown on top of
-#what's onscreen instead of replacing it entirely
+url='https://asdf.com/camera/'
+
+path = xbmc.translatePath('special://profile/addon_data/%s' % __addonid__)
+if not xbmcvfs.exists(path):
+    xbmcvfs.mkdir(path)
+imagefile = os.path.join(path, 'bell.jpg')
+
+
 class CamView(xbmcgui.WindowDialog):
 
     def __init__(self):
-        #Define image location and size
-        self.image = xbmcgui.ControlImage(870, 438, 380, 253, "")
+        #set the initial image before the window is shown
+        urllib.urlretrieve(url, imagefile)
+        self.image = xbmcgui.ControlImage(870, 383, 380, 253, "")
         self.addControl(self.image)
 
 viewer = CamView()
 viewer.show()
 start_time = time.time()
+firstimage = True
 while(time.time() - start_time <= 14):
-    #set url to ip cam image, password auth not supported 
-    urllib.urlretrieve("http://asdf.com/camera/", '/tmp/bell.jpg')
+    urllib.urlretrieve(url, imagefile)
     viewer.image.setImage("")
-    viewer.image.setImage("/tmp/bell.jpg")
-    #Define image transparency 
-    viewer.image.setAnimations([('conditional', 'effect=fade start=90 end=90 time=0 condition=true',)])
+    viewer.image.setImage(imagefile)
+    curr_time = round(time.time() - start_time, 0)
+    if firstimage:
+        nowtime=time.strftime("%I:%M %p")
+        xoptions="Notification(\"Doorbell\",%s, 13800, special://masterprofile/media/bell1.png)" % (nowtime)
+        xbmc.executebuiltin(xoptions)
+        viewer.image.setAnimations([('conditional', 'effect=fade start=0 end=90 time=250 delay=125 condition=true',), ('conditional', 'effect=slide start=0,400 end=0,0 time=250 condition=true',)])
+        firstimage = False
+    elif curr_time == 14:
+        viewer.image.setAnimations([('conditional', 'effect=fade start=90 end=90 time=0 condition=true',), ('conditional', 'effect=slide start=0,0 end=0,400 time=250 condition=true',)])
+        print "catch"
+    else:
+        viewer.image.setAnimations([('conditional', 'effect=fade start=90 end=90 time=0 condition=true',)])
+        print curr_time
     xbmc.sleep(500)
+
+
 viewer.close()
 del viewer
